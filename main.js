0a66a830dca3817b57ba2df5709e10467a91c740
aldeez988 <45502633+aldeez988@users.noreply.github.com>
2019-08-16 19:22:06 +0100

    Update main.js

--- main.js
+++ main.js
@@ -21,14 +21,26 @@ radio.onReceivedString(function (receivedString) {
     handleCollision()
     render()
 })
-function handleCollision () {
-    if (x == xApple && y == yApple) {
-        placeApple()
-    }
-}
+input.onGesture(Gesture.Shake, function () {
+    strip.clear()
+    strip.show()
+})
 function placeApple () {
     xApple = Math.randomRange(0, 7)
     yApple = Math.randomRange(0, 7)
+}
+function render () {
+    strip.clear()
+    strip.setMatrixColor(x, y, neopixel.colors(NeoPixelColors.Green))
+    strip.setMatrixColor(xApple, yApple, neopixel.colors(NeoPixelColors.Red))
+    strip.show()
+}
+function handleCollision () {
+    if (x == xApple && y == yApple) {
+        score = score + 1
+        basic.showNumber(score, 0)
+placeApple()
+    }
 }
 input.onButtonPressed(Button.A, function () {
     radio.sendString("left")
@@ -42,27 +54,20 @@ input.onPinPressed(TouchPin.P1, function () {
 input.onPinPressed(TouchPin.P2, function () {
     radio.sendString("down")
 })
-function render () {
-    strip.clear()
-    strip.setMatrixColor(x, y, neopixel.colors(NeoPixelColors.Green))
-    strip.setMatrixColor(xApple, yApple, neopixel.colors(NeoPixelColors.Red))
-    strip.show()
-}
-input.onGesture(Gesture.Shake, function () {
-    strip.clear()
-    strip.show()
-})
 let yApple = 0
 let xApple = 0
 let y = 0
 let x = 0
 let strip: neopixel.Strip = null
-basic.showString("s")
+let score = 0
+basic.showString("S")
 radio.setGroup(88)
 strip = neopixel.create(DigitalPin.P0, 64, NeoPixelMode.RGB)
 strip.setBrightness(150)
 strip.setMatrixWidth(8)
 x = 0
 y = 0
+score = 0
+basic.showNumber(score, 0)
 placeApple()
 render()
