d04d10bef03bd569a888c8b0fecbb58b27da36e1
Neill Bogie <neillbogie@googlemail.com>
2019-08-16 21:15:07 +0100

    multiple apples, some ts sigs

--- main.js
+++ main.js
@@ -1,73 +1,102 @@
-radio.onReceivedString(function (receivedString) {
-    led.toggle(2, 2)
-    if (receivedString == "left") {
-        led.toggle(0, 2)
-        x += 0 - 1
+function handleCollision() {
+  apples.forEach(a => {
+    if (player.pos.x === a.pos.x && player.pos.y === a.pos.y) {
+      score = score + 1;
+      radio.sendString("ateApple");
+      renderNativeScreen();
+      a.pos = randomMatrixPos();
     }
-    if (receivedString == "right") {
-        led.toggle(4, 2)
-        x += 1
-    }
-    if (receivedString == "up") {
-        led.toggle(2, 0)
-        y += 1
-    }
-    if (receivedString == "down") {
-        led.toggle(2, 4)
-        y += 0 - 1
-    } else {
-    	
-    }
-    handleCollision()
-    render()
-})
-input.onGesture(Gesture.Shake, function () {
-    strip.clear()
-    strip.show()
-})
-function placeApple () {
-    xApple = Math.randomRange(0, 7)
-    yApple = Math.randomRange(0, 7)
+  });
 }
-function render () {
-    strip.clear()
-    strip.setMatrixColor(x, y, neopixel.colors(NeoPixelColors.Green))
-    strip.setMatrixColor(xApple, yApple, neopixel.colors(NeoPixelColors.Red))
-    strip.show()
+function createApples() {
+  apples = [];
+  apples.push(createApple(1));
+  apples.push(createApple(2));
+  apples.push(createApple(3));
+  apples.push(createApple(4));
 }
-function handleCollision () {
-    if (x == xApple && y == yApple) {
-        score = score + 1
-        basic.showNumber(score, 0)
-placeApple()
-    }
+function renderPlayfield() {
+  strip.clear();
+  strip.setMatrixColor(
+    player.pos.x,
+    player.pos.y,
+    neopixel.colors(NeoPixelColors.Green)
+  );
+  apples.forEach(a => {
+    strip.setMatrixColor(a.pos.x, a.pos.y, neopixel.colors(NeoPixelColors.Red));
+  });
+  strip.show();
 }
-input.onButtonPressed(Button.A, function () {
-    radio.sendString("left")
-})
-input.onButtonPressed(Button.B, function () {
-    radio.sendString("right")
-})
-input.onPinPressed(TouchPin.P1, function () {
-    radio.sendString("up")
-})
-input.onPinPressed(TouchPin.P2, function () {
-    radio.sendString("down")
-})
-let yApple = 0
-let xApple = 0
-let y = 0
-let x = 0
-let strip: neopixel.Strip = null
-let score = 0
-basic.showString("S")
-radio.setGroup(88)
-strip = neopixel.create(DigitalPin.P0, 64, NeoPixelMode.RGB)
-strip.setBrightness(150)
-strip.setMatrixWidth(8)
-x = 0
-y = 0
-score = 0
-basic.showNumber(score, 0)
-placeApple()
-render()
+radio.onReceivedString(function(receivedString) {
+  led.toggle(0, 0);
+  if (receivedString == "left") {
+    player.pos.x -= 1;
+  }
+  if (receivedString == "right") {
+    player.pos.x += 1;
+  }
+  if (receivedString == "up") {
+    player.pos.y += 1;
+  }
+  if (receivedString == "down") {
+    player.pos.y -= 1;
+  } else {
+  }
+  handleCollision();
+  renderPlayfield();
+});
+input.onGesture(Gesture.Shake, function() {
+  strip.clear();
+  strip.show();
+});
+function renderNativeScreen() {
+  basic.showNumber(score, 0);
+}
+input.onButtonPressed(Button.A, function() {
+  radio.sendString("left");
+});
+input.onButtonPressed(Button.B, function() {
+  radio.sendString("right");
+});
+input.onPinPressed(TouchPin.P1, function() {
+  radio.sendString("up");
+});
+input.onPinPressed(TouchPin.P2, function() {
+  radio.sendString("down");
+});
+let score = 0;
+let strip: neopixel.Strip = null;
+let player: Player = null;
+let apples: Apple[] = [];
+let numberOfApples = 3;
+function pick(arr: any[]): any {
+  return arr[Math.randomRange(0, arr.length - 1)];
+}
+interface Player {
+  pos: MatrixPos;
+}
+interface Apple {
+  id: number;
+  pos: MatrixPos;
+}
+interface MatrixPos {
+  x: number;
+  y: number;
+}
+function createApple(id: number): Apple {
+  return { id: id, pos: randomMatrixPos() };
+}
+function randomMatrixPos(): MatrixPos {
+  return { x: Math.randomRange(0, 7), y: Math.randomRange(0, 7) };
+}
+createApples();
+basic.showString("S");
+radio.setGroup(88);
+strip = neopixel.create(DigitalPin.P0, 64, NeoPixelMode.RGB);
+strip.setBrightness(110);
+strip.setMatrixWidth(8);
+player = { pos: randomMatrixPos() };
+score = 0;
+renderNativeScreen();
+renderPlayfield();
+radio.sendString("started");
